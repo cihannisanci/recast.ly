9ca318d7ff6147f60666e5ce288636590b2426f4
cihannisanci <cihannisan@gmail.com>
2016-08-12 21:37:46 -0700

    Implement video player

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,34 @@
-var App = () => (
-  <div>
-    <Nav />
-    <div className="col-md-7">
-      <VideoPlayer/>
+class App extends React.Component {
+  
+  constructor() {
+    super();
+    this.state = {
+      currentVideo: exampleVideoData[0]
+    };
+    this.videoClick = this.videoClick.bind(this);
+  }
+
+  videoClick(video) {
+    console.dir(video);
+    this.setState({
+      currentVideo: video
+    });
+  }
+
+  render() {
+    return (
+    <div>
+      <Nav />
+      <div className="col-md-7">
+        <VideoPlayer video={this.state.currentVideo}/>
+      </div>
+      <div className="col-md-5">
+        <VideoList listedVideo={this.videoClick} videos={exampleVideoData}/>
+      </div>
     </div>
-    <div className="col-md-5">
-      <VideoList videos={exampleVideoData}/>
-    </div>
-  </div>
-);
+   );
+  }
+}
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
--- src/components/VideoListEntry.jsx
+++ src/components/VideoListEntry.jsx
@@ -1,20 +1,26 @@
-var VideoListEntry = (props) => (
-  <div className="video-list-entry">
+var VideoListEntry = (props) => {
+
+  return (
+  <div
+    data-video={props.video}
+     className="video-list-entry">
     <div className="media-left media-middle">
-      <img className="media-object"
+      <img className="media-object"  
            src={props.video.snippet.thumbnails.default.url} 
            alt="" />
     </div>
     <div className="media-body">
-      <div className="video-list-entry-title">{props.video.snippet.title}</div>
+      <div onClick={() => props.listedVideo(props.video)} className="video-list-entry-title">{props.video.snippet.title}</div>
       <div className="video-list-entry-detail">{props.video.snippet.description}</div>
     </div>
   </div>
-);
+  );
+};
 // PropTypes tell other developers what `props` a component expects
 // Warnings will be shown in the console when the defined rules are violated
 VideoListEntry.propTypes = {
-  video: React.PropTypes.object.isRequired
+  video: React.PropTypes.object.isRequired,
+  listedVideo: React.PropTypes.func.isRequired
 };
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
