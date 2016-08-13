4dc1e2a7f2bc4605fa3619ac3bf1f46fe010eee8
cihannisanci <cihannisan@gmail.com>
2016-08-12 19:35:22 -0700

    Add video title and description

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,14 +1,9 @@
-var VideoList = () => (
+var VideoList = (props) => (
   <div className="video-list media">
-    <VideoListEntry />
-    <VideoListEntry />
-    <VideoListEntry />
-    <VideoListEntry />
-    <VideoListEntry />
-    <VideoListEntry />
-    <VideoListEntry />
-    <VideoListEntry />
-    <VideoListEntry />
+  {props.videos.map(function(item) {
+    return ( <VideoListEntry video={item} /> );
+  }
+  )}
   </div>
 );
 
